caf88ccdd4ef4a4dd08061bd46b485215544b4f6
Washington <washingtonshiao@gmail.com>
2020-07-15 14:15:21 -0500

    - Updated plot_history utility to plot multiple plots using a list of metric instead of calling it multiple times - Formatted ImageClassificationWithTransferLearning.py with comments and easier to read structure

--- ml_utility.py
+++ ml_utility.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_history(history, epochs, metric, fignum=0):
-    train = history.history[metric]
-    val = history.history[f"val_{metric}"]
+def plot_history(history, epochs, metrics):
+    for fignum, metric in enumerate(metrics):
+        train = history.history[metric]
+        val = history.history[f"val_{metric}"]
 
-    plt.plot(range(1, epochs+1), train, label=f"Training {metric}")
-    plt.plot(range(1, epochs+1), val, label=f"Validation {metric}")
-    plt.legend()
-    plt.figure(fignum)
-    plt.title(f"Training {metric} vs Validation {metric}")
+        plt.figure(fignum)
+        plt.plot(range(1, epochs+1), train, label=f"Training {metric}")
+        plt.plot(range(1, epochs+1), val, label=f"Validation {metric}")
+        plt.legend()
+        plt.title(f"Training {metric} vs Validation {metric}")
 
+    plt.show()
 
 # Samples from a probability distribution, can be tuned with the temperature parameter
 # Higher temperature diversifies output, but can be nonsense
